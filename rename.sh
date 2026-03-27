7a3677a8684b4d13d2cf681a73010b9429c3f990
rDrZ6g2t1ST1 <tigerjonathan9440+l8yat@outlook.com>
2026-03-28 06:09:46 +0700

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,4 +1,4 @@
-rclone lsf mega:udin --include "* 🌌🔥*" | while read f; do
-  new=$(echo "$f" | sed 's/ 🌌🔥//')
+rclone lsf mega:udin --include "*✨*" | while read f; do
+  new=$(echo "$f" | sed 's/✨//')
   rclone moveto "mega:udin/$f" "mega:udin/$new"
 done
